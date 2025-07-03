820ac5cfea9dc7c3d387f333ee16917e86b9f8a0
Gabino Lopez <gabinoelp20@hotmail.com>
2025-07-03 16:49:54 +0200

    db fix

--- database.js
+++ database.js
@@ -8,7 +8,6 @@ require("dotenv").config();
 
 //configuracion de conexion a la base de datos
 const conexion = mysql.createConnection({
-    port: process.env.DB_PORT,
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
