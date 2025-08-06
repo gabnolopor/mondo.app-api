8fc57465b388604d10aeb1485174587e0606690e
Gabino Lopez <gabinoelp20@hotmail.com>
2025-08-06 14:20:08 +0200

    update

--- controller/faciales-controller.js
+++ controller/faciales-controller.js
@@ -3,6 +3,10 @@ const conexion = require("../database");
 const facialesController = {
     // Function to get all faciales
     getFaciales(req, res) {
+        ensureConnection((err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Database connection failed' });
+            }
         let comandoFaciales = "SELECT * FROM ritual_facial";
         conexion.query(comandoFaciales, (err, resultados, campos) => {
             if (err) {
@@ -11,10 +15,15 @@ const facialesController = {
             }
             res.json(resultados).status(200);
         });
+    });
     },
 
     // Function to get the latest faciales
     getLatestFaciales(req, res) {
+        ensureConnection((err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Database connection failed' });
+            }
         let comandoFaciales = "SELECT * FROM ritual_facial ORDER BY id_ritualFacial DESC";
         conexion.query(comandoFaciales, (err, resultados, campos) => {
             if (err) {
@@ -23,10 +32,15 @@ const facialesController = {
             }
             res.json(resultados).status(200);
         });
+    });
     },
 
     // Function to submit a facial form
     facialFormSubmit(req, res) {
+        ensureConnection((err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Database connection failed' });
+            }
         let { nombre, descripcion, precio, precioPareja, duracion } = req.body;
         let comandoForm = "INSERT INTO ritual_facial (nombre_ritualFacial, descripcion_ritualFacial, precio_ritualFacial, precio_ritualFacialPareja, duracion_ritualFacial) VALUES (?, ?, ?, ?, ?)";
         conexion.query(comandoForm, [nombre, descripcion, precio, precioPareja, duracion], (err, resultados) => {
@@ -36,10 +50,15 @@ const facialesController = {
             }
             res.sendStatus(200);
         });
+    });
     },
 
     // Function to update a facial
     updateFacial(req, res) {
+        ensureConnection((err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Database connection failed' });
+            }
         let { id } = req.params;
         let { nombre, descripcion, precio, precioPareja, duracion } = req.body;
         let comandoUpdate = "UPDATE ritual_facial SET nombre_ritualFacial = ?, descripcion_ritualFacial = ?, precio_ritualFacial = ?, precio_ritualFacialPareja = ?, duracion_ritualFacial = ? WHERE id_ritualFacial = ?";
@@ -50,10 +69,15 @@ const facialesController = {
             }
             res.sendStatus(200);
         });
+    });
     },
 
     // Function to delete a facial
     deleteFacial(req, res) {
+        ensureConnection((err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Database connection failed' });
+            }
         let { id } = req.params;
         let comandoDelete = "DELETE FROM ritual_facial WHERE id_ritualFacial = ?";
         conexion.query(comandoDelete, [id], (err, resultados) => {
@@ -63,6 +87,7 @@ const facialesController = {
             }
             res.sendStatus(200);
         });
+    });
     }
 };
 
